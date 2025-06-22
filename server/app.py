14ba6143eee3cebc9a5720b2e5b0332d19f7568b
AliceMugure <machariamugure17@gmail.com>
2025-06-22 11:47:50 +0300

    Initial commit

--- server/app.py
+++ server/app.py
@@ -14,13 +14,36 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+@app.route('/messages', methods=['GET'])
 def messages():
-    return ''
+    messages = Message.query.order_by(Message.created_at.asc()).all()
+    return jsonify([m.to_dict() for m in messages])
 
-@app.route('/messages/<int:id>')
+@app.route('/messages', methods=['POST'])
+def create_message():
+    data = request.get_json()
+    message = Message(body=data['body'], username=data['username'])
+    db.session.add(message)
+    db.session.commit()
+    return jsonify(message.to_dict()), 201
+   
+
+@app.route('/messages/<int:id>', methods=['PATCH'])
 def messages_by_id(id):
-    return ''
+    data = request.get_json()
+    message = Message.query.get_or_404(id)
+    message.body = data['body']
+    db.session.commit()
+    return jsonify(message.to_dict())
+
+@app.route('/messages/<int:id>', methods=['DELETE'])
+def delete_message(id):
+    message = Message.query.get_or_404(id)
+    db.session.delete(message)
+    db.session.commit()
+    return '', 204
+
+
 
 if __name__ == '__main__':
     app.run(port=5555)
